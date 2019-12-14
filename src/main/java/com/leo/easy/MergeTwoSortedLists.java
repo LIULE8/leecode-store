62570e05cf9931380043cff6b6c2dab76f59f312
leo liu <scau_lzc@163.com>
2019-12-14 16:39:45 +0800

    add merge two sorted lists method

---
+++ src/main/java/com/leo/easy/MergeTwoSortedLists.java
@@ -0,0 +1,76 @@
+package com.leo.easy;
+
+import com.leo.structure.ListNode;
+import com.leo.utils.Printer;
+
+/**
+ * 21. 合并两个有序链表
+ * <p>
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode listNode = new Solution().mergeTwoLists(l1, l2);
+        Printer.printListNode(listNode);
+    }
+
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null && l2 == null) {
+                return null;
+            }
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            ListNode p = l1, q = l2, newList;
+            if (p.val >= q.val) {
+                newList = new ListNode(q.val);
+                q = q.next;
+            } else {
+                newList = new ListNode(p.val);
+                p = p.next;
+            }
+            ListNode nexIndex = newList;
+            while (p != null && q != null) {
+                if (p.val >= q.val) {
+                    nexIndex.next = new ListNode(q.val);
+                    q = q.next;
+                } else {
+                    nexIndex.next = new ListNode(p.val);
+                    p = p.next;
+                }
+                nexIndex = nexIndex.next;
+            }
+            while (p != null) {
+                nexIndex.next = new ListNode(p.val);
+                nexIndex = nexIndex.next;
+                p = p.next;
+            }
+
+            while (q != null) {
+                nexIndex.next = new ListNode(q.val);
+                nexIndex = nexIndex.next;
+                q = q.next;
+            }
+            return newList;
+        }
+    }
+}
--- src/main/java/com/leo/utils/Printer.java
+++ src/main/java/com/leo/utils/Printer.java
@@ -1,5 +1,7 @@
 package com.leo.utils;
 
+import com.leo.structure.ListNode;
+
 import java.util.StringJoiner;
 
 public class Printer {
@@ -8,6 +10,16 @@ public class Printer {
         StringJoiner sj = new StringJoiner(",");
         for (int value : a) {
             sj.add(String.valueOf(value));
+        }
+        System.out.println(sj.toString());
+    }
+
+    public static void printListNode(ListNode listNode) {
+        StringJoiner sj = new StringJoiner("->");
+        ListNode indexNode = listNode;
+        while (indexNode != null) {
+            sj.add(String.valueOf(indexNode.val));
+            indexNode = indexNode.next;
         }
         System.out.println(sj.toString());
     }
