fd1c3e34f07d5b1c92ddf3db9a4800401c33c016
leo liu <scau_lzc@163.com>
2019-12-15 16:14:08 +0800

    add databuilder method

---
+++ src/main/java/com/leo/utils/DataBuilder.java
@@ -0,0 +1,21 @@
+package com.leo.utils;
+
+import com.leo.structure.ListNode;
+
+public class DataBuilder {
+
+    /**
+     * @param data data1,data2,data3 ..
+     * @return ListNode head
+     */
+    public static ListNode builderListNode(String data) {
+        String[] datas = data.split(",");
+        ListNode head = new ListNode(Integer.parseInt(datas[0]));
+        ListNode cur = head;
+        for (int i = 1; i < datas.length; i++) {
+            cur.next = new ListNode(Integer.parseInt(datas[i]));
+            cur = cur.next;
+        }
+        return head;
+    }
+}
