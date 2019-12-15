e267aa138f5767cfe7c01de734054c6dcd18e555
leo liu <scau_lzc@163.com>
2019-12-15 16:16:03 +0800

    refactor build listNode method

--- src/main/java/com/leo/easy/MergeTwoSortedLists.java
+++ src/main/java/com/leo/easy/MergeTwoSortedLists.java
@@ -1,6 +1,7 @@
 package com.leo.easy;
 
 import com.leo.structure.ListNode;
+import com.leo.utils.DataBuilder;
 import com.leo.utils.Printer;
 
 /**
@@ -19,12 +20,8 @@ import com.leo.utils.Printer;
  */
 public class MergeTwoSortedLists {
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(2);
-        l1.next.next = new ListNode(4);
-        ListNode l2 = new ListNode(1);
-        l2.next = new ListNode(3);
-        l2.next.next = new ListNode(4);
+        ListNode l1 = DataBuilder.builderListNode("1,2,4");
+        ListNode l2 = DataBuilder.builderListNode("1,3,4");
         ListNode listNode = new Solution().mergeTwoLists(l1, l2);
         Printer.printListNode(listNode);
     }
