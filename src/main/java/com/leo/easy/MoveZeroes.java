8ba5b74af223772bccc786d7febd8c31bd839bfc
Leo Liu <scau_lzc@163.com>
2019-12-16 13:33:48 +0800

    add MoveZeroes method 3

--- src/main/java/com/leo/easy/MoveZeroes.java
+++ src/main/java/com/leo/easy/MoveZeroes.java
@@ -29,7 +29,20 @@ public class MoveZeroes {
   static class Solution {
     public void moveZeroes(int[] nums) {
       //      method1(nums);
-      method2(nums);
+      //      method2(nums);
+      method3(nums);
+    }
+
+    private void method3(int[] nums) {
+      int j = 0;
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          nums[j++] = nums[i];
+        }
+      }
+      for (int i = j; i < nums.length; i++) {
+        nums[i] = 0;
+      }
     }
 
     private void method2(int[] nums) {
