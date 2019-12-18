7c0fdea29281bd4765c4316d8c0a84b36569655a
Leo Liu <scau_lzc@163.com>
2019-12-18 15:26:46 +0800

    add FindPositiveIntegerSolutionForAGivenEquation method

--- src/main/java/com/leo/utils/Printer.java
+++ src/main/java/com/leo/utils/Printer.java
@@ -2,25 +2,30 @@ package com.leo.utils;
 
 import com.leo.structure.ListNode;
 
+import java.util.List;
 import java.util.StringJoiner;
 
 public class Printer {
 
-    public static void printArray(int[] a) {
-        StringJoiner sj = new StringJoiner(",");
-        for (int value : a) {
-            sj.add(String.valueOf(value));
-        }
-        System.out.println(sj.toString());
+  public static void printArray(int[] a) {
+    StringJoiner sj = new StringJoiner(",");
+    for (int value : a) {
+      sj.add(String.valueOf(value));
     }
+    System.out.println(sj.toString());
+  }
 
-    public static void printListNode(ListNode listNode) {
-        StringJoiner sj = new StringJoiner("->");
-        ListNode indexNode = listNode;
-        while (indexNode != null) {
-            sj.add(String.valueOf(indexNode.val));
-            indexNode = indexNode.next;
-        }
-        System.out.println(sj.toString());
+  public static void printListNode(ListNode listNode) {
+    StringJoiner sj = new StringJoiner("->");
+    ListNode indexNode = listNode;
+    while (indexNode != null) {
+      sj.add(String.valueOf(indexNode.val));
+      indexNode = indexNode.next;
     }
+    System.out.println(sj.toString());
+  }
+
+  public static void printDoubleList(List<List<Integer>> list) {
+    System.out.println(list.toString());
+  }
 }
