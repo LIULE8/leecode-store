491617e3ee044a0ba7e32fa87877bf9010587010
leo liu <scau_lzc@163.com>
2019-12-22 11:09:23 +0800

    add a array package

--- src/main/java/com/leo/easy/array/PascalsTriangle.java
+++ src/main/java/com/leo/easy/array/PascalsTriangle.java
@@ -1,10 +1,24 @@
-package com.leo.easy;
+package com.leo.easy.array;
 
 import com.leo.utils.Printer;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 118. 杨辉三角
+ *
+ * <p>给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ *
+ * <p>在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * <p>示例:
+ *
+ * <p>输入: 5 输出: [ [1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1] ]
+ *
+ * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class PascalsTriangle {
   public static void main(String[] args) {
     List<List<Integer>> generate = new Solution().generate(5);
