5697d4da0f2318f934e7f8d35086527b908c502f
leo liu <scau_lzc@163.com>
2019-12-29 19:44:13 +0800

    update BestTimeToBuyAndSellStockli

--- src/main/java/com/leo/easy/array/BestTimeToBuyAndSellStockIi.java
+++ src/main/java/com/leo/easy/array/BestTimeToBuyAndSellStockIi.java
@@ -43,6 +43,11 @@ public class BestTimeToBuyAndSellStockIi {
       return method3(prices);
     }
 
+    /**
+     * 动态规划
+     * @param prices
+     * @return
+     */
     private int method3(int[] prices) {
       if (prices == null || prices.length == 0) return 0;
       int dp[][] = new int[prices.length][2];
