9890ec51fb9d634bd58ac6b85cdd6fbe9061e8cc
Leo <LEO.ZHEN.CONG.LIU@OOCL.COM>
2020-06-19 16:50:53 +0800

    596. 超过5名学生的课

---
+++ src/main/java/com/leo/sql/easy/596_classes_more_than_5_students.sql
@@ -0,0 +1,38 @@
+# 596. 超过5名学生的课
+
+# 有一个courses 表 ，有: student (学生) 和 class (课程)。
+#
+# 请列出所有超过或等于5名学生的课。
+#
+# 例如,表:
+#
+# +---------+------------+
+# | student | class      |
+# +---------+------------+
+# | A       | Math       |
+# | B       | English    |
+# | C       | Math       |
+# | D       | Biology    |
+# | E       | Math       |
+# | F       | Computer   |
+# | G       | Math       |
+# | H       | Math       |
+# | I       | Math       |
+# +---------+------------+
+# 应该输出:
+#
+# +---------+
+# | class   |
+# +---------+
+# | Math    |
+# +---------+
+# Note: 学生在每个课中不应被重复计算。
+
+
+select class
+from courses
+group by class
+having count(distinct student) >= 5;
+
+# 执行用时：310 ms, 在所有 MySQL 提交中击败了11.82%的用户
+# 内存消耗：0B, 在所有 MySQL 提交中击败了100.00%的用户
