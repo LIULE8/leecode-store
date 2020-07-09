f7012261d9c31a9674267d1911f734558e695790
Leo <scau_lzc@163.com>
2020-07-09 13:03:36 +0800

    628. 三个数的最大乘积 --- 超时

--- src/main/java/com/leo/algorithm/easy/array/MaximumProductOfThreeNumbers.java
+++ src/main/java/com/leo/algorithm/easy/array/MaximumProductOfThreeNumbers.java
@@ -21,29 +21,38 @@
 // 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
 //
 // Related Topics 数组 数学
+// 628. 三个数的最大乘积
 
 package com.leo.algorithm.easy.array;
 
 import com.leo.algorithm.utils.DataBuilder;
+import com.leo.algorithm.utils.Printer;
+
+import java.util.Arrays;
 
 public class MaximumProductOfThreeNumbers {
   public static void main(String[] args) {
-    System.out.println(new Solution().maximumProduct(DataBuilder.buildIntArray("1,2,3,4"))); // 24
-    System.out.println(new Solution().maximumProduct(DataBuilder.buildIntArray("1,2,3"))); // 6
-    System.out.println(new Solution().maximumProduct(DataBuilder.buildIntArray("-1,-2,-3"))); // -6
-    System.out.println(
-        new Solution().maximumProduct(DataBuilder.buildIntArray("-4,-3,-2,-1,60"))); // 720
-    System.out.println(
-        new Solution().maximumProduct(DataBuilder.buildIntArray("-4,-3,-2,-1"))); // -6
+    Printer.printCorrectAnswer(
+        "24", new Solution().maximumProduct(DataBuilder.buildIntArray("1,2,3,4")));
+    Printer.printCorrectAnswer(
+        "6", new Solution().maximumProduct(DataBuilder.buildIntArray("1,2,3")));
+    Printer.printCorrectAnswer(
+        "-6", new Solution().maximumProduct(DataBuilder.buildIntArray("-1,-2,-3")));
+    Printer.printCorrectAnswer(
+        "-6", new Solution().maximumProduct(DataBuilder.buildIntArray("-4,-3,-2,-1")));
+    Printer.printCorrectAnswer(
+        "720", new Solution().maximumProduct(DataBuilder.buildIntArray("-4,-3,-2,-1,60")));
   }
 
   static class Solution {
+
     public int maximumProduct(int[] nums) {
       return method1(nums);
     }
 
+
     /**
-     * 冒泡前N个
+     * 超时
      *
      * @param nums
      * @return
@@ -52,18 +61,32 @@ public class MaximumProductOfThreeNumbers {
       if (nums == null || nums.length == 0) {
         return 0;
       }
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < nums.length - 1 - i; j++) {
-          int absNums = Math.abs(nums[j]);
-          int absNextNums = Math.abs(nums[j + 1]);
-          if (absNums > absNextNums) {
-            int temp = nums[j];
-            nums[j] = nums[j + 1];
-            nums[j + 1] = temp;
+      Arrays.sort(nums);
+      int maxMulti = nums[0] * maximumProductOfTwoNumbers(nums, 1, nums[0] < 0);
+      for (int i = 1; i < nums.length - 2; i++) {
+        int temp = nums[i] * maximumProductOfTwoNumbers(nums, i + 1, nums[i] < 0);
+        if (maxMulti < temp) {
+          maxMulti = temp;
+        }
+        // 排除第三个数字重复
+        while (i < nums.length - 2 && nums[i] == nums[i + 1]) i++;
+      }
+      return maxMulti;
+    }
+
+    private int maximumProductOfTwoNumbers(int[] nums, int start, boolean sign) {
+      int multi = nums[start] * nums[start + 1];
+      for (int i = start; i < nums.length - 1; i++) {
+        for (int j = start + 1; j < nums.length; j++) {
+          int temp = nums[i] * nums[j];
+          if (!sign && multi < temp) {
+            multi = temp;
+          } else if (sign && multi > temp) {
+            multi = temp;
           }
         }
       }
-      return nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+      return multi;
     }
   }
 }
