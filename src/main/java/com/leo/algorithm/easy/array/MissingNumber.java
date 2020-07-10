c8d999efaeb73f16806c77440262a7c378618762
Leo <scau_lzc@163.com>
2020-07-10 12:57:52 +0800

    268. 缺失数字 -- 优化

--- src/main/java/com/leo/algorithm/easy/array/MissingNumber.java
+++ src/main/java/com/leo/algorithm/easy/array/MissingNumber.java
@@ -30,6 +30,7 @@ import java.util.Arrays;
 public class MissingNumber {
   public static void main(String[] args) {
     Printer.printCorrectAnswer("1", new Solution().missingNumber(DataBuilder.buildIntArray("0")));
+    Printer.printCorrectAnswer("0", new Solution().missingNumber(DataBuilder.buildIntArray("1")));
     Printer.printCorrectAnswer(
         "8", new Solution().missingNumber(DataBuilder.buildIntArray("9,6,4,2,3,5,7,0,1")));
     Printer.printCorrectAnswer(
@@ -40,7 +41,25 @@ public class MissingNumber {
 
   static class Solution {
     public int missingNumber(int[] nums) {
-      return method1(nums);
+      //      return method1(nums);
+      return method2(nums);
+    }
+
+    /**
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 40.4 MB , 在所有 Java 提交中击败了 6.67% 的用户
+     *
+     * @param nums
+     * @return
+     */
+    private int method2(int[] nums) {
+      if (nums == null || nums.length == 0) return -1;
+      int sum = ((1 + nums.length) * nums.length) >> 1;
+      for (int num : nums) {
+        sum -= num;
+      }
+      return sum == 0 && nums[0] == 0 ? nums.length : sum;
     }
 
     /**
