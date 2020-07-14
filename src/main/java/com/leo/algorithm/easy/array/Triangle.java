b3c441dc299d6fc788a0823f09cb76334495ca3c
Leo <scau_lzc@163.com>
2020-07-14 13:17:57 +0800

    120. 三角形最小路径和

--- src/main/java/com/leo/algorithm/easy/array/Triangle.java
+++ src/main/java/com/leo/algorithm/easy/array/Triangle.java
@@ -38,6 +38,8 @@ public class Triangle {
         new Solution().minimumTotal(DataBuilder.buildDoubleList("[[2],[3,4],[6,5,7],[4,1,8,3]]")));
     Printer.printCorrectAnswer(
         "-1", new Solution().minimumTotal(DataBuilder.buildDoubleList("[[-1],[2,3],[1,-1,-3]]")));
+    Printer.printCorrectAnswer(
+        "12", new Solution().minimumTotal(DataBuilder.buildDoubleList("[[2],[3,4],[6,7,5],[4,1,8,3]]")));
   }
 
   static class Solution {
