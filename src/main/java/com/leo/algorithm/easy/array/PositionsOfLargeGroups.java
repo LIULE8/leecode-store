f2edc0bd932e251bd0f2179aa761e82a38a51e1f
Leo <scau_lzc@163.com>
2020-09-12 21:09:38 +0800

    830. 较大分组的位置

---
+++ src/main/java/com/leo/algorithm/easy/array/PositionsOfLargeGroups.java
@@ -0,0 +1,75 @@
+// 在一个由小写字母构成的字符串 S 中，包含由一些连续的相同字符所构成的分组。
+//
+// 例如，在字符串 S = "abbxxxxzyy" 中，就含有 "a", "bb", "xxxx", "z" 和 "yy" 这样的一些分组。
+//
+// 我们称所有包含大于或等于三个连续字符的分组为较大分组。找到每一个较大分组的起始和终止位置。
+//
+// 最终结果按照字典顺序输出。
+//
+// 示例 1:
+//
+//
+// 输入: "abbxxxxzzy"
+// 输出: [[3,6]]
+// 解释: "xxxx" 是一个起始于 3 且终止于 6 的较大分组。
+//
+//
+// 示例 2:
+//
+//
+// 输入: "abc"
+// 输出: []
+// 解释: "a","b" 和 "c" 均不是符合要求的较大分组。
+//
+//
+// 示例 3:
+//
+//
+// 输入: "abcdddeeeeaabbbcd"
+// 输出: [[3,5],[6,9],[12,14]]
+//
+// 说明: 1 <= S.length <= 1000
+// Related Topics 数组
+// 830. 较大分组的位置
+
+package com.leo.algorithm.easy.array;
+
+import com.leo.algorithm.utils.Printer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PositionsOfLargeGroups {
+  public static void main(String[] args) {
+    Printer.printDoubleList(new Solution().largeGroupPositions("aaa"));
+    Printer.printDoubleList(new Solution().largeGroupPositions("abbxxxxzzy"));
+    Printer.printDoubleList(new Solution().largeGroupPositions("abc"));
+    Printer.printDoubleList(new Solution().largeGroupPositions("abcdddeeeeaabbbcd"));
+  }
+
+  static class Solution {
+    /**
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 39.9 MB , 在所有 Java 提交中击败了 54.09% 的用户
+     *
+     * @param s
+     * @return
+     */
+    public List<List<Integer>> largeGroupPositions(String s) {
+      char[] chars = s.toCharArray();
+      List<List<Integer>> list = new ArrayList<>();
+      int index = 0;
+      for (int i = 1; i < chars.length; i++) {
+        if (chars[index] == chars[i]) continue;
+        if (i - index >= 3) {
+          list.add(Arrays.asList(index, i - 1));
+        }
+        index = i;
+      }
+      if (chars.length - index >= 3) list.add(Arrays.asList(index, chars.length - 1));
+      return list;
+    }
+  }
+}
