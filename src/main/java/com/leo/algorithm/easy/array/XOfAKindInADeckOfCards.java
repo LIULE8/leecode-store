f17b62f1dbeade099f8492a668479d328ad63e11
Leo <scau_lzc@163.com>
2020-09-13 11:20:11 +0800

    914. 卡牌分组

---
+++ src/main/java/com/leo/algorithm/easy/array/XOfAKindInADeckOfCards.java
@@ -0,0 +1,105 @@
+// 给定一副牌，每张牌上都写着一个整数。
+//
+// 此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+//
+//
+// 每组都有 X 张牌。
+// 组内所有的牌上都写着相同的整数。
+//
+//
+// 仅当你可选的 X >= 2 时返回 true。
+//
+//
+//
+// 示例 1：
+//
+// 输入：[1,2,3,4,4,3,2,1]
+// 输出：true
+// 解释：可行的分组是 [1,1]，[2,2]，[3,3]，[4,4]
+//
+//
+// 示例 2：
+//
+// 输入：[1,1,1,2,2,2,3,3]
+// 输出：false
+// 解释：没有满足要求的分组。
+//
+//
+// 示例 3：
+//
+// 输入：[1]
+// 输出：false
+// 解释：没有满足要求的分组。
+//
+//
+// 示例 4：
+//
+// 输入：[1,1]
+// 输出：true
+// 解释：可行的分组是 [1,1]
+//
+//
+// 示例 5：
+//
+// 输入：[1,1,2,2,2,2]
+// 输出：true
+// 解释：可行的分组是 [1,1]，[2,2]，[2,2]
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= deck.length <= 10000
+// 0 <= deck[i] < 10000
+//
+//
+//
+// Related Topics 数组 数学
+// 914. 卡牌分组
+
+package com.leo.algorithm.easy.array;
+
+import com.leo.algorithm.utils.DataBuilder;
+import com.leo.algorithm.utils.Printer;
+
+import java.util.Arrays;
+
+public class XOfAKindInADeckOfCards {
+  public static void main(String[] args) {
+    Printer.printCorrectAnswer(
+        true, new Solution().hasGroupsSizeX(DataBuilder.buildIntArray("1,2,3,4,4,3,2,1")));
+    Printer.printCorrectAnswer(
+        false, new Solution().hasGroupsSizeX(DataBuilder.buildIntArray("1,1,1,2,2,2,3,3")));
+    Printer.printCorrectAnswer(
+        false, new Solution().hasGroupsSizeX(DataBuilder.buildIntArray("1")));
+    Printer.printCorrectAnswer(
+        true, new Solution().hasGroupsSizeX(DataBuilder.buildIntArray("1,1")));
+    Printer.printCorrectAnswer(
+        true, new Solution().hasGroupsSizeX(DataBuilder.buildIntArray("1,1,2,2,2,2")));
+    Printer.printCorrectAnswer(
+        true, new Solution().hasGroupsSizeX(DataBuilder.buildIntArray("0,0,0,1,1,1,2,2,2")));
+  }
+
+  static class Solution {
+    /**
+     * 执行用时： 9 ms , 在所有 Java 提交中击败了 35.66% 的用户
+     *
+     * <p>内存消耗： 40.6 MB , 在所有 Java 提交中击败了 19.60% 的用户
+     *
+     * @param deck
+     * @return
+     */
+    public boolean hasGroupsSizeX(int[] deck) {
+      int[] counter = new int[10000];
+      for (int index : deck) {
+        counter[index]++;
+      }
+      return Arrays.stream(counter).reduce(this::gcd).getAsInt() > 1;
+    }
+
+    private int gcd(int a, int b) {
+      return b == 0 ? a : gcd(b, a % b);
+    }
+  }
+}
