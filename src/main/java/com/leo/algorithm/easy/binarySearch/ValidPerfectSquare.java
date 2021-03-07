2ba9a1d8d0c0cc49e0377036731bc6c37b9b0ff9
Leo <scau_lzc@163.com>
2021-03-07 10:56:17 +0800

    367. 有效的完全平方数

--- src/main/java/com/leo/algorithm/easy/binarySearch/ValidPerfectSquare.java
+++ src/main/java/com/leo/algorithm/easy/binarySearch/ValidPerfectSquare.java
@@ -28,22 +28,56 @@ public class ValidPerfectSquare {
     Printer.printCorrectAnswer(true, new Solution().isPerfectSquare(0));
     Printer.printCorrectAnswer(true, new Solution().isPerfectSquare(16));
     Printer.printCorrectAnswer(false, new Solution().isPerfectSquare(14));
+    Printer.printCorrectAnswer(true, new Solution().isPerfectSquare(808201));
   }
 
   static class Solution {
     public boolean isPerfectSquare(int num) {
-      return method1(num);
+      //      return method1(num);
+      return method2(num);
+    }
+
+    /**
+     * 記得用long 和 num 需要先 除以 2 否則精度丟失以及超時
+     *
+     * <p>执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 35.3 MB , 在所有 Java 提交中击败了 32.55% 的用户
+     *
+     * @param num
+     * @return
+     */
+    private boolean method2(int num) {
+      if (num == 1) return true;
+      long l = 0, r = num >> 1;
+      while (l <= r) {
+        long mid = l + ((r - l) >> 1);
+        long res = mid * mid;
+        if (res == num) {
+          return true;
+        } else if (res < num) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+      return false;
     }
 
     /**
      * 牛頓迭代法
+     *
+     * <p>执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 35.2 MB , 在所有 Java 提交中击败了 32.55% 的用户
+     *
      * @param num
      * @return
      */
     private boolean method1(int num) {
       long r = num;
       while (r * r > num) {
-        r = (r + num /r) /2;
+        r = (r + num / r) / 2;
       }
       return r * r == num;
     }
