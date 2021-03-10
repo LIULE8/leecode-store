a995ee679a35813e424a4efdc44ac4345d327933
Leo <scau_lzc@163.com>
2021-03-10 12:50:11 +0800

    33. 搜索旋转排序数组

--- src/main/java/com/leo/algorithm/easy/binarySearch/XuanZhuanShuZuDeZuiXiaoShuZiLcof.java
+++ src/main/java/com/leo/algorithm/easy/binarySearch/XuanZhuanShuZuDeZuiXiaoShuZiLcof.java
@@ -57,13 +57,9 @@ public class XuanZhuanShuZuDeZuiXiaoShuZiLcof {
       int l = 0, r = numbers.length - 1;
       while (l <= r) {
         int mid = l + ((r - l) >> 1);
-        if (numbers[mid] > numbers[r]) {
-          l = mid + 1;
-        } else if (numbers[mid] < numbers[r]) {
-          r = mid;
-        } else {
-          r = r - 1;
-        }
+        if (numbers[mid] > numbers[r]) l = mid + 1;
+        else if (numbers[mid] < numbers[r]) r = mid;
+        else r--;
       }
       return numbers[l];
     }
--- src/main/java/com/leo/algorithm/middle/binarySearch/SearchInRotatedSortedArray.java
+++ src/main/java/com/leo/algorithm/middle/binarySearch/SearchInRotatedSortedArray.java
@@ -53,27 +53,56 @@ import com.leo.algorithm.utils.Printer;
 
 public class SearchInRotatedSortedArray {
   public static void main(String[] args) {
-//    Printer.printCorrectAnswer(
-//        4, new Solution().search(DataBuilder.buildIntArray("4,5,6,7,0,1,2"), 0));
-//    Printer.printCorrectAnswer(
-//        -1, new Solution().search(DataBuilder.buildIntArray("4,5,6,7,0,1,2"), 3));
-//    Printer.printCorrectAnswer(-1, new Solution().search(DataBuilder.buildIntArray("1"), 0));
-//    Printer.printCorrectAnswer(-1, new Solution().search(DataBuilder.buildIntArray("1,3"), 2));
-//    Printer.printCorrectAnswer(1, new Solution().search(DataBuilder.buildIntArray("1,3"), 3));
-//    Printer.printCorrectAnswer(1, new Solution().search(DataBuilder.buildIntArray("3,1"), 1));
+    Printer.printCorrectAnswer(
+        4, new Solution().search(DataBuilder.buildIntArray("4,5,6,7,0,1,2"), 0));
+    Printer.printCorrectAnswer(
+        -1, new Solution().search(DataBuilder.buildIntArray("4,5,6,7,0,1,2"), 3));
+    Printer.printCorrectAnswer(-1, new Solution().search(DataBuilder.buildIntArray("1"), 0));
+    Printer.printCorrectAnswer(-1, new Solution().search(DataBuilder.buildIntArray("1,3"), 2));
+    Printer.printCorrectAnswer(1, new Solution().search(DataBuilder.buildIntArray("1,3"), 3));
+    Printer.printCorrectAnswer(1, new Solution().search(DataBuilder.buildIntArray("3,1"), 1));
     Printer.printCorrectAnswer(0, new Solution().search(DataBuilder.buildIntArray("5,1,3"), 5));
   }
 
   static class Solution {
 
+    /**
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 37.7 MB , 在所有 Java 提交中击败了 77.45% 的用户
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
-      int l = 0, r = nums.length - 1;
+      int startIndex = getMinNumsIndex(nums);
+      int index = binarySearch(nums, startIndex, nums.length - 1, target);
+      if (index == -1) {
+        index = binarySearch(nums, 0, startIndex, target);
+      }
+      return index;
+    }
+
+    private int binarySearch(int[] nums, int l, int r, int target) {
       while (l <= r) {
         int mid = l + ((r - l) >> 1);
         if (nums[mid] == target) return mid;
-
+        else if (nums[mid] < target) l = mid + 1;
+        else r = mid - 1;
       }
       return -1;
+    }
+
+    private int getMinNumsIndex(int[] nums) {
+      int l = 0, r = nums.length - 1;
+      while (l <= r) {
+        int mid = l + ((r - l) >> 1);
+        if (nums[mid] > nums[r]) l = mid + 1;
+        else if (nums[mid] < nums[r]) r = mid;
+        else r--;
+      }
+      return l;
     }
   }
 }
