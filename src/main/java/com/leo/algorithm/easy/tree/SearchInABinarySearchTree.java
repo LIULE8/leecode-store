7701de76d9d36add04b0ca0222bc3b7b42005462
Leo <scau_lzc@163.com>
2021-03-20 10:01:50 +0800

    700. 二叉搜索树中的搜索

---
+++ src/main/java/com/leo/algorithm/easy/tree/SearchInABinarySearchTree.java
@@ -0,0 +1,66 @@
+// 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+//
+// 例如，
+//
+//
+// 给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+// 和值: 2
+//
+//
+// 你应该返回如下子树:
+//
+//
+//      2
+//     / \
+//    1   3
+//
+//
+// 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+// Related Topics 树
+// 700. 二叉搜索树中的搜索
+
+package com.leo.algorithm.easy.tree;
+
+import com.leo.algorithm.structure.TreeNode;
+
+public class SearchInABinarySearchTree {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+  }
+
+  static class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+      //      return method1(root, val);
+      return method2(root, val);
+    }
+
+    private TreeNode method2(TreeNode root, int val) {
+      if (root == null || root.val == val) return root;
+      return val > root.val ? searchBST(root.right, val) : searchBST(root.left, val);
+    }
+
+    /**
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 7.25% 的用户
+     *
+     * <p>内存消耗： 39.4 MB , 在所有 Java 提交中击败了 47.89% 的用户
+     *
+     * @param root
+     * @param val
+     * @return
+     */
+    private TreeNode method1(TreeNode root, int val) {
+      if (root == null) return null;
+      if (root.val == val) return root;
+      TreeNode left = searchBST(root.left, val);
+      TreeNode right = searchBST(root.right, val);
+      return left != null ? left : right;
+    }
+  }
+}
