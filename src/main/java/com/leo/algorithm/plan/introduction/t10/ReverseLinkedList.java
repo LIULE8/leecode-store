02d51bdd08b4d4b7f48459b5065f9bfa20ddeb4c
Leo <scau_lzc@163.com>
2021-07-21 13:06:01 +0800

    206. 反转链表

---
+++ src/main/java/com/leo/algorithm/plan/introduction/t10/ReverseLinkedList.java
@@ -0,0 +1,74 @@
+// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+// 输入：head = [1,2,3,4,5]
+// 输出：[5,4,3,2,1]
+//
+//
+// 示例 2：
+//
+//
+// 输入：head = [1,2]
+// 输出：[2,1]
+//
+//
+// 示例 3：
+//
+//
+// 输入：head = []
+// 输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目范围是 [0, 5000]
+// -5000 <= Node.val <= 5000
+//
+//
+//
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+//
+//
+// Related Topics 递归 链表
+// 206. 反转链表
+
+package com.leo.algorithm.plan.introduction.t10;
+
+import com.leo.algorithm.structure.ListNode;
+
+public class ReverseLinkedList {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+  }
+
+  static class Solution {
+    /**
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 38.3 MB , 在所有 Java 提交中击败了 40.60% 的用户
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+      ListNode newHead = null;
+      ListNode p = head;
+      while (p != null) {
+        ListNode cur = new ListNode(p.val);
+        cur.next = newHead;
+        newHead = cur;
+        p = p.next;
+      }
+      return newHead;
+    }
+  }
+}
