b76914faa37a14aa90ad392d39f3c3c27219bf72
Leo <scau_lzc@163.com>
2021-10-30 21:32:47 +0800

    链表学习--合并有序链表

--- src/main/java/com/leo/algorithm/easy/linklist/MergeTwoSortedLists.java
+++ src/main/java/com/leo/algorithm/easy/linklist/MergeTwoSortedLists.java
@@ -50,6 +50,60 @@ public class MergeTwoSortedLists {
   }
 
   static class Solution {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+      //      return method1(l1, l2);
+      return method2(l1, l2);
+    }
+
+    /**
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * <p>内存消耗： 37.7 MB , 在所有 Java 提交中击败了 79.85% 的用户
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    private ListNode method2(ListNode l1, ListNode l2) {
+      // 边界校验
+      if ((l1 == null && l2 == null)) return l1;
+      if (l2 != null && l1 == null) return l2;
+      if (l2 == null) return l1;
+      // 假头
+      ListNode newNode = new ListNode(0);
+      ListNode p = l1;
+      ListNode q = l2;
+      ListNode x = newNode;
+      while (p != null && q != null) {
+        if (p.val > q.val) {
+          x.next = new ListNode(q.val);
+          q = q.next;
+        } else if (p.val < q.val) {
+          x.next = new ListNode(p.val);
+          p = p.next;
+        } else {
+          x.next = new ListNode(p.val);
+          p = p.next;
+          x = x.next;
+          x.next = new ListNode(q.val);
+          q = q.next;
+        }
+        x = x.next;
+      }
+      while (p != null) {
+        x.next = new ListNode(p.val);
+        p = p.next;
+        x = x.next;
+      }
+      while (q != null) {
+        x.next = new ListNode(q.val);
+        q = q.next;
+        x = x.next;
+      }
+      return newNode.next;
+    }
+
     /**
      * 执行用时： 1 ms , 在所有 Java 提交中击败了 84.93% 的用户
      *
@@ -59,7 +113,7 @@ public class MergeTwoSortedLists {
      * @param l2
      * @return
      */
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    private ListNode method1(ListNode l1, ListNode l2) {
       ListNode head = new ListNode(0); // 设立头节点
       ListNode tail = head;
       while (l1 != null && l2 != null) {
