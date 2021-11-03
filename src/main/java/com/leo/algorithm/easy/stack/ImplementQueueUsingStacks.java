2c0790d3740ee7e51c6a9de26e95274216444d8f
Leo <scau_lzc@163.com>
2021-11-03 22:30:27 +0800

    栈学习-用栈实现队列--方式二

--- src/main/java/com/leo/algorithm/easy/stack/ImplementQueueUsingStacks.java
+++ src/main/java/com/leo/algorithm/easy/stack/ImplementQueueUsingStacks.java
@@ -116,9 +116,51 @@ public class ImplementQueueUsingStacks {
   }
 
   /**
-   * Your MyQueue object will be instantiated and called as such: MyQueue obj = new MyQueue();
-   * obj.push(x); int param_2 = obj.pop(); int param_3 = obj.peek(); boolean param_4 = obj.empty();
+   * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+   *
+   * <p>内存消耗： 36.3 MB , 在所有 Java 提交中击败了 25.03% 的用户
    */
-  // leetcode submit region end(Prohibit modification and deletion)
+  static class MyQueue2 {
 
+    private final Stack<Integer> stack1;
+    private final Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue2() {
+      stack1 = new Stack<>();
+      stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+      stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+      if (!stack2.empty()) {
+        return stack2.pop();
+      }
+      while (!stack1.empty()) {
+        stack2.push(stack1.pop());
+      }
+      return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+      if (!stack2.empty()) {
+        return stack2.peek();
+      }
+      while (!stack1.empty()) {
+        stack2.push(stack1.pop());
+      }
+      return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+      return stack1.empty() && stack2.empty();
+    }
+  }
 }
